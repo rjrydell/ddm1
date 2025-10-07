867ce34e8fd2b4252cb70ab4f31f1873e1b6a4b2
rjrydell <rjrydell@iu.edu>
2025-10-07 10:47:40 -0400

    Update gcatangry1.js

--- gcatangry1.js
+++ gcatangry1.js
@@ -61,7 +61,7 @@ define(['pipAPI', 'https://rjrydell.github.io/ddm1/jamp.js'], function(APIConstr
 				nameForLogging : 'fix2', //Will be used in the logging
 				//An array of all media objects for this category.
 				mediaArray : [
-				    	{word : '+'}, 
+				    {word : '+'}, 
 					{word : '+'},
 					{word : '+'},
 					{word : '+'},
@@ -270,7 +270,7 @@ define(['pipAPI', 'https://rjrydell.github.io/ddm1/jamp.js'], function(APIConstr
 		primeDuration : 200, 
 		postPrimeDuration : 75, //Duration of blank screen between prime and target.
 		targetDuration : 500, //Duration of target presentation.
-		ITI : 1000, //Duration between trials.
+		ITI : 750, //Duration between trials.
 
 		//  ********* END REAL TRIALS **********//
 
